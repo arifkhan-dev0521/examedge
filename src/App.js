1569a034b246684e1df092bf0d1774f5ba482cf6
arifkhan-dev0521 <arifkhan97281@gmail.com>
2026-07-05 22:59:07 +0530

    Connected SubjectPage to Firestore database

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
       <ScrollToTop />
       {/* <FloatingBack /> */}
       <Navbar theme={theme} toggleTheme={toggleTheme} />
-      <button 
+      {/* <button 
   onClick={async () => {
     const result = await uploadAllSubjects();
     alert(result.success ? `Uploaded ${result.count} subjects!` : `Error: ${result.error}`);
@@ -47,7 +47,7 @@ function App() {
   style={{ position: 'fixed', top: '70px', right: '10px', zIndex: 999, padding: '10px', background: 'green', color: 'white', border: 'none', borderRadius: '8px' }}
 >
   Upload to Firebase
-</button>
+</button> */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/kuk" element={<CoursePage />} />
